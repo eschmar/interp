e7f59904efc6512269a9f5f5394ab7608ed72632
Marcel Eschmann <marcel.eschmann@gmail.com>
2020-08-31 14:31:09 +0200

    Basic structure

--- example.cpp
+++ example.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include "interp.h"
+#include <iomanip>
 
 int main() {
     std::cout << "Hello from interp! " << interp_VERSION_SEMANTIC << "\n";
 
     pew::Interp p = pew::interp(1000);
+    p.setOnStep([](double value){
+        std::cout.precision(17);
+        std::cout << "Val: " << std::fixed << value << ". Tick.\n";
+    });
+
+    p.run();
 
     return 0;
 }
--- src/interp.h
+++ src/interp.h
@@ -1,14 +1,62 @@
 #ifndef PEW_INTERP_H
 #define PEW_INTERP_H
 
+#include <functional>
+#include <iostream>
+#include <chrono>
+#include <unistd.h>
+#include <limits>
+#include <algorithm>
+
 #define interp_VERSION_SEMANTIC "1.0.0"
 
 namespace pew {
     class Interp {
+        int frequencyHz = 60;
+        uint64_t start = 0, duration = 400;
+
+        std::function<void(double)> onStep;
+        std::function<void()> onStart, onEnd;
+        std::function<double(double)> easing;
+
+        inline uint64_t now() {
+            return std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::system_clock::now().time_since_epoch()
+            ).count();
+        }
+
         Interp();
+        void tick() {
+            uint64_t elapsed = this->now() - start;
+            double ratio = std::clamp((double) elapsed / duration, 0.0, 1.0);
+
+            // apply easing function if available, otherwise linear
+            if (easing) ratio = easing(ratio);
+
+            // allow user defined behaviour on step
+            if (onStep) onStep(ratio);
+
+            // check if end is reached
+            if (ratio >= 1.0) {
+                if (onEnd) onEnd();
+                return;
+            }
+
+            usleep(1000000.0 / frequencyHz);
+            tick();
+        }
 
         public:
             static Interp create(long duration);
+            void setOnStart(std::function<void()> fun) { onStart = fun; };
+            void setOnEnd(std::function<void()> fun) { onEnd = fun; };
+            void setOnStep(std::function<void(double)> fun) { onStep = fun; };
+
+            void run() {
+                start = now();
+                if (onStart) onStart();
+                tick();
+            }
     };
 
     inline Interp interp(long duration) {
